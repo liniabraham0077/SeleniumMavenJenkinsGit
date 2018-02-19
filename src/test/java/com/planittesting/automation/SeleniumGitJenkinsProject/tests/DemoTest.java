702cbb75bbc6938e99866344349c3b6e7e04d67a
Lini Abraham <labraham@planittesting.com>
2018-02-19 16:30:29 +1100

    initial commit

---
+++ src/test/java/com/planittesting/automation/SeleniumGitJenkinsProject/tests/DemoTest.java
@@ -0,0 +1,14 @@
+package com.planittesting.automation.SeleniumGitJenkinsProject.tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void mytest() {
+		
+		System.out.println("This is a test 111");
+		
+	}
+
+}
