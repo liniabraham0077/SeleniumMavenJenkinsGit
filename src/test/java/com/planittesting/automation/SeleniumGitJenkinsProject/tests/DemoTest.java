b022b4cf90ef977340fbf3cc35b020dd09e3a0e0
Lini Abraham <labraham@planittesting.com>
2018-02-20 13:42:13 +1100

    added a new test

--- src/test/java/com/planittesting/automation/SeleniumGitJenkinsProject/tests/DemoTest.java
+++ src/test/java/com/planittesting/automation/SeleniumGitJenkinsProject/tests/DemoTest.java
@@ -5,9 +5,17 @@ import org.testng.annotations.Test;
 public class DemoTest {
 	
 	@Test
-	public void mytest() {
+	public void mytest1() {
 		
-		System.out.println("This is a test 111");
+		System.out.println("This is test 1");
+		
+	}
+	
+
+	@Test
+	public void mytest2() {
+		
+		System.out.println("This is test 2");
 		
 	}
 
