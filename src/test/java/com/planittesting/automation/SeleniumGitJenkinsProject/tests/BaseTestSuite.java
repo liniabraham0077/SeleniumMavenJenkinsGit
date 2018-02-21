19821f0cf65594c704d40c5ca4b80e5e3e3a3c55
Lini Abraham <labraham@planittesting.com>
2018-02-21 15:39:22 +1100

    added extent reports

--- src/test/java/com/planittesting/automation/SeleniumGitJenkinsProject/tests/BaseTestSuite.java
+++ src/test/java/com/planittesting/automation/SeleniumGitJenkinsProject/tests/BaseTestSuite.java
@@ -2,10 +2,14 @@ package com.planittesting.automation.SeleniumGitJenkinsProject.tests;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -13,6 +17,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -26,9 +31,33 @@ public abstract class BaseTestSuite {
 	ExtentHtmlReporter htmlReporter;
 	ExtentReports reports;
 
+	@Parameters(value={"browser","version","platform"})
 	@BeforeClass
-	public static void setup() {
-		driver = new ChromeDriver();
+	public static void setup(String browser, String version, String platform) throws MalformedURLException {
+		if (browser.equals("chrome")) {
+
+			DesiredCapabilities capability = new DesiredCapabilities();
+			capability.setCapability("platform", platform);
+			capability.setCapability("browserName", browser);
+			capability.setCapability("browserVersion", version);
+			capability.setCapability("project", "P1");
+			capability.setCapability("build", "1.0");
+
+			driver = new RemoteWebDriver(new URL("http://192.168.56.1:4548/wd/hub"), capability);
+		}
+		else if (browser.equals("firefox")) {
+
+			DesiredCapabilities capability = new DesiredCapabilities();
+			capability.setCapability("platform", platform);
+			capability.setCapability("browserName", browser);
+			capability.setCapability("browserVersion", version);
+			capability.setCapability("project", "P1");
+			capability.setCapability("build", "1.0");
+			capability.setCapability("marionette", true);
+
+			driver = new RemoteWebDriver(new URL("http://192.168.56.1:4549/wd/hub"), capability);
+		}
+
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 	}
